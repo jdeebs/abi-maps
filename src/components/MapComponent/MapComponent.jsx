b552930051a1436a023e1cb605c450d870173b3f
Jacob DiBiase <jakesback93@hotmail.com>
2025-04-11 13:45:38 -0700

    Add inline component to apply fitBounds to Map Component

--- src/components/MapComponent/MapComponent.jsx
+++ src/components/MapComponent/MapComponent.jsx
@@ -1,4 +1,5 @@
-import { MapContainer, ImageOverlay } from 'react-leaflet';
+import { MapContainer, ImageOverlay, useMap } from 'react-leaflet';
+import { useEffect } from 'react';
 import L from "leaflet";
 
 import 'leaflet/dist/leaflet.css';
@@ -13,6 +14,21 @@ function MapComponent() {
         [0, 0], // Top-left corner (y, x)
         [1833, 3028], // Bottom-right corner (y, x)
     ];
+
+    // Inline component to apply fitBounds
+    const CenterMap = () => {
+        // Leaflet hook to get access to map instance
+        const map = useMap();
+
+        // Runs after MapComponent is added to the DOM
+        useEffect(() => {
+            map.fitBounds(valleyBounds);
+        }, [map]);
+
+        // Component doesn't render anything visually
+        // so just returns null
+        return null;
+    }
   
     return (
         <MapContainer
@@ -29,6 +45,7 @@ function MapComponent() {
             url={valleyMapImage}
             bounds={valleyBounds}
         />
+        <CenterMap />
         </MapContainer>
     );
 }
